487b58f814b216fcb1c71e9ef7ec3d2643f47206
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-02-11 23:42:39 -0500

    Fix bugs and handle edge cases

--- script.py
+++ script.py
@@ -8,21 +8,30 @@ def main():
     if workflow_path.is_dir():
         printed_header = False
         repo = environ.get("REPO")
+        repo_name = repo.split("/")[1]
         for workflow in workflow_path.iterdir():
             if workflow.suffix == ".yml" and workflow.is_file():
                 with workflow.open() as f:
                     workflow_dict = safe_load(f)
-                    if on_dict := workflow_dict.get("on", {}):
+                    if on_dict := workflow_dict.get(True, {}):
                         not_excluding_main_branch = False
-                        if branches := on_dict.get("branches", []):
-                            not_excluding_main_branch = "!main" not in branches and "!master" not in branches
-                        if branches_ignore := on_dict.get("branches-ignore", []):
-                            not_excluding_main_branch = "main" not in branches_ignore and "master" not in branches_ignore
-                        if on_dict.get("schedule", []):
-                            not_excluding_main_branch = True
+                        if isinstance(on_dict, str):
+                            not_excluding_main_branch = on_dict == "push"
+                        elif isinstance(on_dict, list):
+                            not_excluding_main_branch = "push" in on_dict
+                        else:
+                            if on_push_duct := on_dict.get("push", {}):
+                                if branches := on_push_duct.get("branches", []):
+                                    not_excluding_main_branch = "!main" not in branches and "!master" not in branches
+                                else:
+                                    not_excluding_main_branch = True
+                                if branches_ignore := on_push_duct.get("branches-ignore", []):
+                                    not_excluding_main_branch = "main" not in branches_ignore and "master" not in branches_ignore
+                            if on_dict.get("schedule", []):
+                                not_excluding_main_branch = True
                         if not_excluding_main_branch:
                             if not printed_header:
-                                print(f"## {repo}")
+                                print(f"## [{repo_name}](https://github.com/{repo})")
                                 printed_header = True
                             name = workflow_dict.get("name", workflow.stem)
                             print(f"[![{name}](https://github.com/{repo}/actions/workflows/{workflow.name}/badge.svg)](https://github.com/{repo}/actions/workflows/{workflow.name})", end=" ")
